4ac5f53647abae3fea33e890faf7d1b7d52de716
Andrew Cline <andrew.cline77@gmail.com>
2018-11-22 13:00:11 -0600

    chore: setup test utils

--- client-app/src/tests/App.test.tsx
+++ client-app/src/tests/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from '../components/App'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('<App />', () => {
+  it('should match snaptshot', () => {
+    const wrap = shallow(<App />)
+    expect(toJson(wrap)).toMatchSnapshot()
+  })
 })

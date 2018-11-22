3394bbf4de04fca85245daa7cb51e7103119c4da
Andrew Cline <andrew.cline77@gmail.com>
2018-11-22 12:45:49 -0600

    chore: update app struture

---
+++ client-app/src/components/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import '../css/App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <h1>John 1</h1>
+        <p>
+          In the beginning, there was the Word. The Word was with God and the
+          word was God.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App

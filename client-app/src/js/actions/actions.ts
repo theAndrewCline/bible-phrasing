8e2be8991fc3f4a74c110ae22e90a0c4cc19154f
Andrew Cline <andrew.cline77@gmail.com>
2018-11-30 10:49:55 -0600

    feat: complete fetch function

--- client-app/src/js/actions/actions.ts
+++ client-app/src/js/actions/actions.ts
@@ -1,47 +1,21 @@
 import * as types from './actionTypes'
-import { string } from 'prop-types'
 
-interface Iparams {
-  q: string
-  'indent-poetry': boolean
-  'include-headings': boolean
-  'include-footnotes': boolean
-  'include-verse-numbers': boolean
-  'include-short-copyright': boolean
-  'include-passage-references': boolean
+export const fetchPassage = (passageQuery: string) => {
+    const API_KEY = process.env.ESV_API_TOKEN
+    const URL: string = `https://api.esv.org/v3/passage/text/?q=${passageQuery}&include-footnotes=False&include-headings=False&include-passage-references=False&include-short-copyright=False`
+    let passage
+
+
+    fetch(URL, {
+    headers: {
+        Authorization: 'Token' + API_KEY
+    }
+    })
+    .then(response => response.json())
+    .then(json => {
+        passage = json.passage
+    })
+
+    return passage
 }
 
-const API_KEY = process.env.ESV_API_TOKEN
-
-const params = {
-  q: string,
-  'indent-poetry': false,
-  'include-headings': false,
-  'include-footnotes': false,
-  'include-verse-numbers': false,
-  'include-short-copyright': false,
-  'include-passage-references': false
-}
-
-// const url = (urlParams: any) => {
-//   let originalUrl = new URL('https://api.esv.org/v3/passage/text')
-//   originalUrl.searchParams = new URLSearchParams(urlParams)
-// }
-
-// export const receiveVerse = json => {
-//   return { type: types.RECEIVE_VERSES, verses: json }
-// }
-
-// export const fetchVerse = () => {
-//   return dispatch => {
-//     return fetch(url(params), {
-//       method: 'GET',
-//       mode: 'cors',
-//       credentials: 'include',
-//       cache: 'default',
-//       headers: {
-//         Authorization: 'Token' + API_KEY
-//       }
-//     })
-//   }
-// }
---
+++ client-app/src/tests/fetchPassage.test.ts
@@ -0,0 +1,7 @@
+import { fetchPassage } from '../js/actions/actions'
+
+describe('fetchPassage()', () => {
+    it('should be a function', () => {
+        expect(fetchPassage).toBeDefined()
+    })
+})

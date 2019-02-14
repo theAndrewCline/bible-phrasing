20ba2ea10d6545943bea5e418e44ce590991ae4a
Andrew Cline <andrew.cline77@gmail.com>
2019-02-13 22:04:57 -0600

    feat: WIP featpassage

--- client-app/src/components/App.tsx
+++ client-app/src/components/App.tsx
@@ -3,19 +3,32 @@ import NavBar from './NavBar'
 import '../css/App.css'
 import { Provider } from 'react-redux'
 import { Store } from 'redux'
+import { fetchPassage } from '../js/actions/actions';
 
 // const store: Store = createStore()
 
-class App extends Component {
-  render () {
+class App extends Component<{}, {passage: string}> {
+  constructor (props: any) {
+    super(props)
+
+    this.state = {
+      passage: ''
+    }
+  }
+
+  public componentWillMount () {
+    fetchPassage('John+3:16').then(passage => { 
+      this.setState({passage: passage.passages[0]})
+    })
+  }
+  public render () {
     return (
       // <Provider store={store}>
         <div className='App'>
           <NavBar />
           <h1>John 1</h1>
           <p>
-            In the beginning, there was the Word. The Word was with God and the
-            word was God.
+            {this.state.passage}
           </p>
         </div>
       // </Provider>
--- client-app/src/js/actions/actions.ts
+++ client-app/src/js/actions/actions.ts
@@ -1,21 +1,23 @@
 import * as types from './actionTypes'
 
-export const fetchPassage = (passageQuery: string) => {
-    const API_KEY = process.env.ESV_API_TOKEN
-    const URL: string = `https://api.esv.org/v3/passage/text/?q=${passageQuery}&include-footnotes=False&include-headings=False&include-passage-references=False&include-short-copyright=False`
-    let passage
-
-
-    fetch(URL, {
-    headers: {
-        Authorization: 'Token' + API_KEY
-    }
-    })
-    .then(response => response.json())
-    .then(json => {
-        passage = json.passage
-    })
-
-    return passage
+interface Passage {
+    query: string
+    canonical: string
+    parsed: any[]
+    passage_meta: any[]
+    passages: any[]
 }
 
+export const fetchPassage = (passageQuery: string): Promise<Passage> => {
+    const API_KEY = process.env.ESV_API_TOKEN
+    const URL: string = `https://api.esv.org/v3/passage/text/?q=${passageQuery}&include-footnotes=False&include-headings=False&include-passage-references=False&include-short-copyright=False`
+
+    return fetch(URL, {
+        headers: {
+            Authorization: 'Token ' + API_KEY,
+            mode: 'no-cors'
+        }
+    })
+        .then(response => response.json())
+        .then(json => json.passage)
+}

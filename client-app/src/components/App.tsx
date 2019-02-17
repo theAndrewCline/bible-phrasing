cb46da8d7bc3ced1dfbc89e34667a30baa5900fb
Andrew <andrew.cline77@gmail.com>
2019-02-16 19:49:50 -0600

    chore: env isssue

--- client-app/src/components/App.tsx
+++ client-app/src/components/App.tsx
@@ -7,29 +7,27 @@ import { fetchPassage } from '../js/actions/actions';
 
 // const store: Store = createStore()
 
-class App extends Component<{}, {passage: string}> {
+class App extends Component<{}, {passages: any[]}> {
   constructor (props: any) {
     super(props)
 
     this.state = {
-      passage: ''
+      passages: []
     }
   }
 
   public componentWillMount () {
-    fetchPassage('John+3:16').then(passage => { 
-      this.setState({passage: passage.passages[0]})
+    fetchPassage('John+3:16-18').then(response => { 
+      this.setState({passages: response })
     })
   }
   public render () {
     return (
       // <Provider store={store}>
-        <div className='App'>
-          <NavBar />
-          <h1>John 1</h1>
-          <p>
-            {this.state.passage}
-          </p>
+      <div className='App'>
+        <NavBar />
+        <h1>John 1</h1>
+        {this.state.passages.map((passage) => <p>{passage}</p>)}
         </div>
       // </Provider>
     )
